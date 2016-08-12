42f6d91df3ff5749050b8a35648c0bd3c489a4a7
Chas Emerick <chas@pdfdata.io>
2016-08-11 23:39:28 -0400

    document upload, metadata retrieval, listing

--- test/pdfdata.spec.js
+++ test/pdfdata.spec.js
@@ -1,9 +1,12 @@
 var apikey = process.env.PDFDATA_APIKEY;
 var endpoint = process.env.PDFDATA_ENDPOINT || "https://api.pdfdata.io/v1"
 
-var assert = require('chai').assert
+var assert = require('chai').assert;
+var path = require("path");
 
 assert.isDefined(apikey);
+
+console.log("Testing with endpoint: " + endpoint);
 
 var pdfdata = require("../lib/pdfdata")(apikey);
 pdfdata.setEndpoint(endpoint);
@@ -15,4 +18,37 @@ it("info", function (done) {
 	assert.match(result.build, /^[\da-f]{40}$/);
 	done();
     });
+});
+
+function isID (prefix, id) {
+    assert.match(id, new RegExp("^" + prefix + "_" + "[\\da-f]+$"));
+}
+
+function checkDate (datestr) {
+    assert.equal(datestr.length, 20);
+    assert.instanceOf(new Date(datestr), Date);
+}
+
+it("upload one", function (done) {
+    var file = "package.json";
+    var document;
+    pdfdata.documents.upload(file)
+	.then(function (result) {
+	    assert.equal(result.length, 1);
+	    document = result[0];
+	    isID("doc", document.id);
+	    assert.equal(document.type, "doc");
+	    assert.equal(document.filename, path.basename(file))
+	    checkDate(document.created);
+	    checkDate(document.expires);
+	    return pdfdata.documents.get(document.id);
+	})
+	.then(function (result) {
+	    assert.deepEqual(document, result);
+	    return pdfdata.documents.list();
+	})
+	.then(function (result) {
+	    assert.include(result, document);
+	    done();
+	});
 });
