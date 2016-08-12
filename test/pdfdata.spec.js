42efe09a0dfc865d7582a5b44ecf46901c2d180b
Chas Emerick <chas@pdfdata.io>
2016-08-12 01:10:06 -0400

    document resource impl done

--- test/pdfdata.spec.js
+++ test/pdfdata.spec.js
@@ -1,8 +1,10 @@
 var apikey = process.env.PDFDATA_APIKEY;
 var endpoint = process.env.PDFDATA_ENDPOINT || "https://api.pdfdata.io/v1"
 
+var Promise = require("bluebird");
 var assert = require('chai').assert;
 var path = require("path");
+var fs = require("fs");
 
 assert.isDefined(apikey);
 
@@ -20,6 +22,10 @@ it("info", function (done) {
     });
 });
 
+var pdfs = fs.readdirSync("pdfs").map(function (filename) {
+    return path.join("pdfs", filename);
+});
+
 function isID (prefix, id) {
     assert.match(id, new RegExp("^" + prefix + "_" + "[\\da-f]+$"));
 }
@@ -29,26 +35,68 @@ function checkDate (datestr) {
     assert.instanceOf(new Date(datestr), Date);
 }
 
-it("upload one", function (done) {
-    var file = "package.json";
-    var document;
-    pdfdata.documents.upload(file)
-	.then(function (result) {
-	    assert.equal(result.length, 1);
-	    document = result[0];
-	    isID("doc", document.id);
-	    assert.equal(document.type, "doc");
-	    assert.equal(document.filename, path.basename(file))
-	    checkDate(document.created);
-	    checkDate(document.expires);
-	    return pdfdata.documents.get(document.id);
-	})
-	.then(function (result) {
-	    assert.deepEqual(document, result);
-	    return pdfdata.documents.list();
-	})
-	.then(function (result) {
-	    assert.include(result, document);
-	    done();
+describe("document management", function () {
+    it("upload one", function (done) {
+	var file = pdfs[0];
+	var document;
+	pdfdata.documents.upload(file)
+	    .then(function (result) {
+		assert.equal(result.length, 1);
+		document = result[0];
+		isID("doc", document.id);
+		assert.equal(document.type, "doc");
+		assert.equal(document.filename, path.basename(file))
+		checkDate(document.created);
+		checkDate(document.expires);
+		return pdfdata.documents.get(document.id);
+	    })
+	    .then(function (result) {
+		assert.deepEqual(document, result);
+		return pdfdata.documents.list();
+	    })
+	    .then(function (result) {
+		assert.include(result, document);
+		done();
+	    });
+    });
+
+    it("upload many", function (done) {
+	var documents;
+	var filenames = pdfs.map(function (p) {
+	    return path.basename(p);
 	});
+	var tags = ["test-tag", "Group123"];
+	pdfdata.documents.upload(pdfs, tags)
+	    .then(function (result) {
+		assert.equal(result.length, pdfs.length);
+		documents = result;
+		documents.forEach(function (document) {
+		    isID("doc", document.id);
+		    assert.equal(document.type, "doc");
+		    assert.include(filenames, document.filename);
+		    checkDate(document.created);
+		    checkDate(document.expires);
+		    tags.forEach(function (tag) {
+			assert.include(document.tags, tag);
+		    });
+		});
+
+		return Promise.map(documents, function (document) {
+		    return pdfdata.documents.get(document.id);
+		});
+	    })
+	    .then(function (result) {
+		documents.forEach(function (document) {
+		    assert.include(result, document);
+		});
+
+                return pdfdata.documents.list();
+	    })
+	    .then(function (result) {
+		documents.forEach(function (document) {
+		    assert.include(result, document);
+		});
+		done();
+	    }).catch(console.log);
+    });
 });
